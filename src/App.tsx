0ea28be79dbe636e53ee059206fd6b78e96057fe
Jeremy May <may.jeremy.r@gmail.com>
2022-06-03 09:49:12 -0700

    chore: vite init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Row, Typography } from "web3uikit";
+
+function App() {
+  return (
+    <>
+      <Row alignItems="center" justifyItems="center">
+        <Typography variant="h1">NFT Forum</Typography>
+      </Row>
+      <Row alignItems="center" justifyItems="center">
+        <Typography variant="body18">
+          Demo app that will support NFT avatars and login using NFTs
+        </Typography>
+      </Row>
+    </>
+  );
+}
+
+export default App;
